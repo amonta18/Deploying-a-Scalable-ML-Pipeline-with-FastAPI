ecab3e7a3c29d95954aa637f61f6b23670b1a489
amonta18 <amonta18@wgu.edu>
2026-06-27 20:04:40 +0000

    Add unit tests and screenshot

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,49 @@
-import pytest
+
 # TODO: add necessary import
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+from ml.model import (
+    train_model,
+    inference,
+    compute_model_metrics,
+)
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_model():
     """
-    # add description for the first test
+    # Test that train_model returns a RandomForestClassifier.
     """
-    # Your code here
-    pass
+    X_train = np.array([[1, 2], [3, 4], [5, 6]])
+    y_train = np.array([0, 1, 0])
+    
+    model = train_model(X_train, y_train)
 
+    assert isinstance(model, RandomForestClassifier)
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_inference():
     """
-    # add description for the second test
+    Test that inference returns the correct number of predictions.
     """
-    # Your code here
-    pass
+    X_train = np.array([[1, 2], [3, 4], [5, 6]])
+    y_train = np.array([0, 1, 0])
 
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_train)
+
+    assert len(preds) == len(y_train)
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics():
     """
-    # add description for the third test
+    Test that the metric values are between 0 and 1.
     """
-    # Your code here
-    pass
+    y = np.array([1, 0, 1, 1])
+    preds = np.array([1, 0, 0, 1])
+    
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert 0 <= precision <= 1
+    assert 0 <= recall <= 1
+    assert 0 <= fbeta <= 1
